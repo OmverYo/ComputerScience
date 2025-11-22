3b6645b935d616fdbac4af7460ada8285e603ed9
Omver <justinlee990106@gmail.com>
2025-11-22 20:36:21 +0900

    C 포인터 주소의 대해 더 공부하고 *있고 차이점 알아냄

--- C Coding/pointer.c
+++ C Coding/pointer.c
@@ -21,5 +21,34 @@ int main()
     int *myNum;
     */
 
+    int myNumbers[4] = {10, 20, 30, 40};
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("%p\n", &myNumbers[i]); // Outputs the memory addresses of each element
+    }
+
+    int myInt;
+    printf("Size of myInt: %zu bytes\n", sizeof(myInt));   // Outputs size of int (typically 4 bytes)
+    printf("Size of myNumbers: %zu\n", sizeof(myNumbers)); // Outputs size of the entire array (16 bytes for 4 ints)
+    printf("%p\n", myNumbers);                             // Outputs the memory address of the array (same as &myNumbers[0])
+    printf("%p\n", &myNumbers[0]);                         // Outputs the memory address of the first element
+    printf("%d\n", *myNumbers);                            // Outputs the value of the first element (10)
+    printf("%d\n", *(myNumbers + 1));                      // Outputs the value of the second element (20)
+    printf("%d\n", *(myNumbers + 2));                      // Outputs the value of the third element (30)
+    printf("%d\n", *(myNumbers + 3));                      // Outputs the value of the fourth element (40)
+
+    int *pointer = myNumbers;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%d\n", *(pointer + i)); // Outputs each element of the array using pointer arithmetic
+    }
+
+    *myNumbers = 13;
+    *(myNumbers + 1) = 17;
+    printf("%d\n", *myNumbers);       // Outputs 13
+    printf("%d\n", *(myNumbers + 1)); // Outputs 17
+
     return 0;
 }
